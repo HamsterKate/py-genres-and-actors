391cd764fb1b27df565517c73c398f525dbfcb3b
Kate <hamster.k.po@gmail.com>
2026-03-22 18:47:03 +0200

    Solution genres and actors

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
+from django.template.defaultfilters import first
+
 import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
 
+from db.models import *
+
 
 def main() -> QuerySet:
-    pass
+    for g_name in ["Western", "Action", "Dramma"]:
+        genre = Genre.objects.create(name=g_name)
+
+    actors_data = [
+        {"first_name": "George", "last_name": "Klooney"},
+        {"first_name": "Kianu", "last_name": "Reaves"},
+        {"first_name": "Scarlett", "last_name": "Keegan"},
+        {"first_name": "Will", "last_name": "Smith"},
+        {"first_name": "Jaden", "last_name": "Smith"},
+        {"first_name": "Scarlett", "last_name": "Johansson"},
+    ]
+
+    for actor in actors_data:
+        Actor.objects.create(**actor)
+
+    Genre.objects.filter(name="Dramma").update(name="Drama")
+    Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(last_name="Reaves").update(
+        first_name="Keanu",
+        last_name="Reeves"
+    )
+
+    Genre.objects.filter(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
+
+
+if __name__ == "__main__":
+    print(main())
