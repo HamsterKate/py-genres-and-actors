78ff0296c75591ad5280683ccc9daf52858db1ed
Kate <hamster.k.po@gmail.com>
2026-03-22 18:57:14 +0200

    actors dict to tuple

--- main.py
+++ main.py
@@ -12,16 +12,17 @@ def main() -> QuerySet:
         genre = Genre.objects.create(name=g_name)
 
     actors_data = [
-        {"first_name": "George", "last_name": "Klooney"},
-        {"first_name": "Kianu", "last_name": "Reaves"},
-        {"first_name": "Scarlett", "last_name": "Keegan"},
-        {"first_name": "Will", "last_name": "Smith"},
-        {"first_name": "Jaden", "last_name": "Smith"},
-        {"first_name": "Scarlett", "last_name": "Johansson"},
+        ("George", "Klooney"),
+        ("Kianu", "Reaves"),
+        ("Scarlett", "Keegan"),
+        ("Will", "Smith"),
+        ("Jaden", "Smith"),
+        ("Scarlett", "Johansson"),
     ]
 
-    for actor in actors_data:
-        Actor.objects.create(**actor)
+    for first_name, last_name in actors_data:
+        Actor.objects.create(first_name=first_name,
+                             last_name=last_name)
 
     Genre.objects.filter(name="Dramma").update(name="Drama")
     Actor.objects.filter(last_name="Klooney").update(last_name="Clooney")
@@ -34,7 +35,3 @@ def main() -> QuerySet:
     Actor.objects.filter(first_name="Scarlett").delete()
 
     return Actor.objects.filter(last_name="Smith").order_by("first_name")
-
-
-if __name__ == "__main__":
-    print(main())
